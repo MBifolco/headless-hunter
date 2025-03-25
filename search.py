300942c330a508f206ae4ae5b4ae5567f6133431
Michael Bifolco <mike@bifolco.com>
2025-03-24 23:53:29 -0400

    add greenhouse

--- search.py
+++ search.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 from modules.db import create_db, save_job
 from modules.api.consider import ConsiderApiSite
 from modules.selenium.getro import GetroSeleniumSite
+from modules.api.greenhouse import GreenhouseApiSite
 from pprint import pprint as pp
 
 # Load environment variables from a local .env file
@@ -23,6 +24,7 @@ APP_CONFIG = {
 SCRAPER_CLASSES = {
     "consider": ConsiderApiSite,
     "getro": GetroSeleniumSite,
+    "greenhouse": GreenhouseApiSite,
 }
 
 
@@ -35,16 +37,20 @@ with open('test_site_configs.json') as f:
 #load consider configs
 with open('consider_sites.json') as f:
     SITE_CONFIGS.extend(json.load(f))
+    # load greenhouse sites from file
+with open('greenhouse_sites.json') as f:
+    SITE_CONFIGS.extend(json.load(f))
 # load sites.json from file
 with open('getro_sites.json') as f:
     SITE_CONFIGS.extend(json.load(f))
+
 
 # ====================
 # Main Processing
 # ====================
 def main():
     conn = create_db()
-    
+    total_jobs_checked = 0
     for site_config in SITE_CONFIGS:
         site_type = site_config.pop("type")
         site_name = site_config.get("name")
@@ -58,9 +64,10 @@ def main():
         scraper = scraper_class(app_config = APP_CONFIG, **site_config)
     
         jobs = scraper.scrape()
-    
+        
         if jobs is not None:
-            print(f"Found {len(jobs)} jobs for {site_name}.")
+            total_jobs_checked += len(jobs)
+            print(f"Found {len(jobs)} jobs for {site_name}. Total jobs checked: {total_jobs_checked}")
             saved = 0
             for job in jobs:
                 if scraper.should_save_job(job):
