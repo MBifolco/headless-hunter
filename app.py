061fa2940ccd7b19e36f1589cbf0a926dcfacc2f
Michael Bifolco <mike@bifolco.com>
2025-03-31 14:10:21 -0400

    sort

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ templates = Jinja2Templates(directory="templates")
 def get_jobs():
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
-    cursor.execute("SELECT job_id, title, company_name, apply_url, location_city, location_state, location_country, remote, hybrid, last_seen, site_id, id FROM jobs GROUP BY job_id ORDER BY company_name, title")
+    cursor.execute("SELECT job_id, title, company_name, apply_url, location_city, location_state, location_country, remote, hybrid, last_seen, site_id, id FROM jobs GROUP BY job_id ORDER BY last_seen DESC, company_name, title")
     jobs = cursor.fetchall()
     conn.close()
     return jobs
